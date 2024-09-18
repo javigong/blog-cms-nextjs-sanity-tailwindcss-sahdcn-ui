c62fba76fe007be6c3b51c13fb6db174e807e468
Javier Gongora <42308135+javigong@users.noreply.github.com>
2024-09-18 14:29:45 -0700

    Add blog article

--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -1,8 +1,15 @@
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { PortableTextBlock } from "next-sanity";
 
 export interface simpleBlogCard {
   title: string;
   smallDescription: string;
+  currentSlug: string;
+  titleImage: SanityImageSource;
+}
+export interface blogArticle {
+  title: string;
+  content: PortableTextBlock;
   currentSlug: string;
   titleImage: SanityImageSource;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import Link from "next/link";
 import { simpleBlogCard } from "./lib/interface";
 import { client, urlFor } from "./lib/sanity";
-import Image from "next/image";
 
 async function getData() {
   const query = `*[_type == 'blog'] | order(_createdAt desc) {
@@ -18,16 +20,28 @@ async function getData() {
 export default async function Home() {
   const data: simpleBlogCard[] = await getData();
 
-  console.log(data)
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-4 mt-5">
-      {
-        data.map((post,idx)=>(
-          <Card key={idx}>
-            <Image src={urlFor(post.titleImage).url()} alt={post.title} width={250} height={250} />
-          </Card>
-        ))
-      }
+    <div className="grid grid-cols-1 lg:grid-cols-2 mt-5 gap-5">
+      {data.map((post, idx) => (
+        <Card key={idx}>
+          <Image
+            src={urlFor(post.titleImage).url()}
+            alt={post.title}
+            width={500}
+            height={500}
+            className="rounded-t-lg h-[200px] object-cover"
+          />
+          <CardContent className="mt-5">
+            <h3 className="text-lg font-bold line-clamp-2">{post.title}</h3>
+            <p className="text-sm line-clamp-3 mt-2 text-gray-600 dark:text-gray-300">
+              {post.smallDescription}
+            </p>
+            <Button asChild className="w-full mt-7">
+              <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
+            </Button>
+          </CardContent>
+        </Card>
+      ))}
     </div>
   );
 }
