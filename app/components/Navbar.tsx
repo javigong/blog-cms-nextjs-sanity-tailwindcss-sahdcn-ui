a49a96053887d01a044ab194954d03965e804bf1
Javier Gongora <42308135+javigong@users.noreply.github.com>
2024-09-17 22:31:09 -0700

    Initial configuration

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
+     <Link className="font-bold text-3xl" href={"/"}>NextDev<span className="text-primary">Blog</span></Link>
+    </nav>
+  );
+};
+
+export default Navbar;
